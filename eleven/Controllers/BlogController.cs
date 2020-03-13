f6c6acfd4885cdb78bec33e926f0b1dd8eb54b25
ancient-lunatic <60133632+ancient-lunatic@users.noreply.github.com>
2020-03-13 13:54:29 +0530

    Add files via upload

---
+++ eleven/Controllers/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eleven.Controllers
+{
+    [Route("api/[controller]")]
+    public class BlogController : ControllerBase
+    {
+        public BlogController(appDb db)
+        {
+            Db = db;
+        }
+
+        // GET api/blog/user
+ 
+
+        [HttpGet("user")]
+        public async Task<IActionResult> GetUserInfo()
+        {
+            await Db.Connection.OpenAsync();
+            var query = new employee(Db);
+            var result = await query.getUserInformation();
+            return new OkObjectResult(result);
+        }
+        public appDb Db { get; }
+    }
+
+}
---
+++ eleven/employee.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+using eleven.Models;
+using eleven.NewFolder;
+using MySql.Data.MySqlClient;
+
+
+namespace eleven
+{
+    public class employee
+    {
+        
+        public appDb Db { get; }
+       
+        public employee(appDb db)
+        {
+            Db = db;
+        }
+
+
+
+        // querry execute
+        public async Task<List<employees>> getUserInformation()
+        {
+            using var cmd = Db.Connection.CreateCommand();
+            cmd.CommandText = @"select  CONCAT(firstName , ' ' , middleName , ' ' , lastname) as Name,countryName as nationality,
+CONCAT(addressLine1 , ',' , addressLine2 , ' ,' , cityName,',',stateName,',',pincode)as address,GROUP_CONCAT(contact_information.contactNumber) as `numbers`,
+TIMESTAMPDIFF(YEAR,dateOfJoing,CURDATE()) as experienceyaer, TIMESTAMPDIFF( MONTH, dateOfJoing, now() ) % 12 as currentExperiencemonth,
+    TIMESTAMPDIFF ( YEAR, birthDate, now() ) as year
+    , TIMESTAMPDIFF( MONTH, birthDate, now() ) % 12 as month
+    , ( TIMESTAMPDIFF( DAY, birthDate, now() ) % 30 ) as day
+FROM employee,address,join_date,city,country,contact_information,state where employee.employeeId=address.addressId and
+address.cityId=city.cityId and employee.employeeId=join_date.employeeId and employee.nationality=country.countryId and state.stateId=city.stateId
+ and employee.employeeId=contact_information.employeeId group by
+  employee.employeeId;";
+            return await ReadAllEmployee(await cmd.ExecuteReaderAsync());
+        }
+
+
+      
+        
+        //mapping 
+        
+        public async Task<List<employees>> ReadAllEmployee(DbDataReader reader)
+        {
+            var posts = new List<employees>();   // create an array of blogpost
+            using (reader)
+            {
+                while (await reader.ReadAsync())
+                {
+                    var post = new employees()
+                    {
+                       
+                        Name = reader.GetString(0),
+                        nationality = reader.GetString(1),
+                        Address = reader.GetString(2),
+                        contactDetail = new contactInformation(reader.GetString(3)),
+                        currentCompanyExp = reader.GetString(4)+ " years " + reader.GetString(5) + " months",
+                        Age = new Models.dateOfBirth(reader.GetString(6) , reader.GetString(7) , reader.GetString(8)),
+                       // DOJ = reader.GetString(7)
+
+                    };
+                    posts.Add(post);
+                }
+            }
+            return posts;
+        }
+
+      
+    }
+}
